c3efc4780486888f3efadbfec6fa298fd8837fe9
hi <MyAppLe1DD@pm.me>
2026-05-18 18:12:12 +0400

    Remove Lovable project leftovers

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import legacy from "@vitejs/plugin-legacy";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -21,7 +20,6 @@ export default defineConfig(({ mode }) => ({
         modernPolyfills: true,
         renderLegacyChunks: true,
       }),
-    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
